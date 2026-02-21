44c06a4df31f13bbe4cd5ba7cebe716d14ed8791
Kaoh <fedenox0@gmail.com>
2026-02-20 23:24:51 -0300

    fix render port

--- src/server.js
+++ src/server.js
@@ -259,6 +259,8 @@ app.get("/communities/:id/feed", async (req, res) => {
   }
 })
 
-app.listen(5000, () => {
-  console.log("Servidor corriendo en http://localhost:5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en puerto", PORT)
 })
